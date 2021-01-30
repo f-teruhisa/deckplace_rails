75b773935c6e0ae7a549bbff83cf6894db607365
f-teruhisa <teru_fukumoto@outlook.jp>
2021-01-30 16:01:24 +0900

    [Add] Instllation Tailwind CSS
    Note: https://dev.to/vonagedev/using-tailwind-css-with-vue-js-b1b

--- config/application.rb
+++ config/application.rb
@@ -35,14 +35,14 @@ module App
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
-  end
-end
 
-# Rspec configure
-config.generators do |g|
-  g.test_framework :rspec,
-                   view_specs: false,
-                   helper_specs: false,
-                   controller_specs: false,
-                   routing_specs: false
+    # Rspec configure
+    config.generators do |g|
+      g.test_framework :rspec,
+                       view_specs: false,
+                       helper_specs: false,
+                       controller_specs: false,
+                       routing_specs: false
+    end
+  end
 end
